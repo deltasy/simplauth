890f20865757bf6f91bc85cce1e533c5b44a9ef9
Resloko <duckedevel@gmail.com>
2026-09-06 19:09:05 -0300

    feat: initial (safe)

--- src/shared/middleware/error.middleware.ts
+++ src/shared/middleware/error.middleware.ts
@@ -2,15 +2,17 @@ import type { NextFunction, Request, Response } from "express"
 import { Prisma } from "@prisma/client";
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
-    if(error instanceof Prisma.PrismaClientKnownRequestError){
-        if(error.code === "P2002"){
-            return res.status(409).json({error: "Este registro já está em uso."})
+    // Erros neutros que não entregam informações desnecessárias para usuários mal intencionados
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+            return res.status(409).json({ error: "Os dados fornecidos entram em conflito com um registro existente." });
         }
-        if(error.code === "P2025"){
-            return res.status(404).json({error: "Registro não encontrado."})
+
+        if (error.code === "P2025") {
+            return res.status(404).json({ error: "O recurso solicitado não foi encontrado ou não existe." });
         }
     }
 
-    console.error("[ERRO CRÍTICO]", error);
+    console.error("[ERRO INTERNO]:", error);
     return res.status(500).json({ error: "Erro interno do servidor." });
 }
