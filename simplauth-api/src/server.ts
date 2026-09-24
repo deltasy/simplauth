fef23db472bda49dde1a6b60cde88d37cd65dde0
Resloko <duckedevel@gmail.com>
2026-09-24 09:26:42 -0300

    feat: ready for run version

--- simplauth-api/src/server.ts
+++ simplauth-api/src/server.ts
@@ -34,7 +34,7 @@ app.use(cors({
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
     optionsSuccessStatus: 200
-}))
+}));
 
 app.use(express.json());
 app.use(cookieParser());
